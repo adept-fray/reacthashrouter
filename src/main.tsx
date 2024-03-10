3944437cacd7db17568a22449a4e31e4d0fb602e
adept fray <siavash.kh41@gmail.com>
2024-03-10 09:16:48 +0100

    deploy workflows all

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./assets/pages/Home.tsx";
+import Contact from "./assets/pages/Contact.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/reacthashrouter/",
+    element: <App />,
+    children: [
+      {
+        path: "/reacthashrouter/",
+        element: <Home />,
+      },
+      {
+        path: "/reacthashrouter/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
